9c3d293b3e34e1699bc0ff8de5b683083b558890
Sivakumarkalaiselvan <sivakumarkalaiselvan5@gmail.com>
2025-01-29 19:04:41 +0530

    Revert "Terraform testing"
    This reverts commit 4461837c2e664ca6d15c42de8f16db7cdd6e617f.

--- C5-RG.tf
+++
@@ -1,7 +0,0 @@
-# Resource Group
-
-resource "azurerm_resource_group" "myrg" {
-  name = "${local.resource_group_prefix}-${var.resource_group_name}-${random_string.myrandom.id}"
-  location = var.resource_group_location
-  tags = local.common_tags
-}
--- c4-RandomString.tf
+++
@@ -1,6 +0,0 @@
-resource "random_string" "myrandom" {
-  length = 6
-  upper = false
-  lower = false
-  special = false
-}
